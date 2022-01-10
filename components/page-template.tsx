73fdd994104389c5df909b1d09bc473cd3134877
Nolan Pather <nolan@luno.com>
2022-01-10 14:34:52 +0200

    basic page layout

--- components/page-template.tsx
+++ components/page-template.tsx
@@ -1,6 +1,8 @@
 import { Entry } from 'contentful'
 import React from 'react'
 
+import { Box, Container } from '@mui/material'
+
 import { Page } from '../lib/models/page'
 import { ArticleTemplate } from './article-template'
 import { NavigationListTemplate } from './navigation-list-template'
@@ -12,21 +14,35 @@ interface PageProps {
 export const PageTemplate: React.FC<PageProps> = ({ page }) => {
   const { fields } = page
   return (
-    <>
-      <header>
+    <Container maxWidth="md">
+      <Box
+        component="header"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexFlow: 'column nowrap',
+        }}
+      >
         <h1>{fields.title}</h1>
         <h2>{fields.subtitle}</h2>
-      </header>
-      <main>
-        {fields.main.map((article) => (
-          <ArticleTemplate article={article} key={article.sys.id} />
-        ))}
-      </main>
-      <aside>
-        {fields.aside.map((nav) => (
-          <NavigationListTemplate key={nav.sys.id} nav={nav} />
-        ))}
-      </aside>
-    </>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexFlow: 'row nowrap',
+        }}
+      >
+        <main>
+          {fields.main.map((article) => (
+            <ArticleTemplate article={article} key={article.sys.id} />
+          ))}
+        </main>
+        <aside>
+          {fields.aside.map((nav) => (
+            <NavigationListTemplate key={nav.sys.id} nav={nav} />
+          ))}
+        </aside>
+      </Box>
+    </Container>
   )
 }
